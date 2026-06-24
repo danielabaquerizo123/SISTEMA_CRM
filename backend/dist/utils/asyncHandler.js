24b53d029ed8ecd2512caa3ea4118923f35194cd
Daniela Baquerizo <dnbaquerizom@fafi.utb.edu.ec>
2026-06-23 19:02:19 -0500

    Eliminar node_modules y archivos sensibles del control de versiones

--- backend/dist/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = asyncHandler;
-function asyncHandler(controller) {
-    return (req, res, next) => {
-        controller(req, res, next).catch(next);
-    };
-}
